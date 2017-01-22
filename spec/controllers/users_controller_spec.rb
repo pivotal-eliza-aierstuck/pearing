495143e291f9e45d3e8597ee713549f80ce25feb
Devin Morgenstern <devin.morgenstern@gmail.com>
2017-01-21 22:42:44 -0500

    Clean up User model spec and delete unused UsersController spec

--- spec/controllers/users_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper" do
-
-  describe UsersController do
-
-    describe ""
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 describe User do
-  it "has a full_name" do
-    name = "Test Name"
-    user = User.new(full_name: name)
+  it "has a full_name and level" do
+    name = "Jane Smith"
+    skill_level = "Sr. Developer"
+
+    user = User.new(full_name: name, level: skill_level)
+
     expect(user.full_name).to eq(name)
+    expect(user.level).to eq(skill_level)
   end
 end
